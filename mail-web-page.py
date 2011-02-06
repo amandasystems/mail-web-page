92470d2f9b4d5bee769d2c624d42074ffe73f3f1
Albin Stjerna <albin@eval.nu>
2011-02-06 16:10:21 +0100

    Embed images using base64 encoding.

--- mail-web-page.py
+++ mail-web-page.py
@@ -2,7 +2,7 @@
 # -*- encoding:utf-8 -*-
 
 from BeautifulSoup import BeautifulSoup as bs
-import urllib2, sys, re, os, imp, urlparse
+import urllib2, sys, re, os, imp, urlparse, base64, mimetypes
 from email.mime.text import MIMEText
 
 # init conf object
@@ -17,6 +17,11 @@ def filter_soup(rules, url, soup):
     return (False, soup) # no rule matched: return input data and a flag telling so.
 
 def format_html_message(url, rules, soup):
+
+    def base64encode_image(fp, mime, charset):
+        data = base64.encodestring(fp.read())
+        return "data:%s;charset=%s;base64,%s" % (mime, charset, data)
+
     contents = filter_soup(rules, url, soup)[1]
     try:
         title = soup.head.title.contents[0].encode("utf-8")
@@ -29,6 +34,18 @@ def format_html_message(url, rules, soup):
         if oldlink and oldlink[:1] == "/":
             href = urlparse.urljoin(url, oldlink)
             a['href'] = href
+
+    # base64-encode and embed images into HTML
+    for img in contents.findAll("img"):
+        if img['src'][:1] == "/":
+            imgurl = urlparse.urljoin(url, img['src'])
+        else:
+            imgurl = img['src']
+        op = urllib2.build_opener()
+        op.addheaders = [('User-agent', 'Mozilla/5.0')]
+        imgdata = op.open(imgurl)
+        imgtype, imgenc = mimetypes.guess_type(imgurl)
+        img['src'] = base64encode_image(imgdata, imgtype, "utf-8")
         
     return '<h1><a href="%s">%s</h1></a></h1>' % \
         (url, title) \
